d8cb53e07a898f0c305ef2a00dae7f43bd2618ca
“Debora <debora.crescenzo@gmail.com>
2021-01-27 21:08:48 +0000

    tag

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view/>
+  <router-view  />
 </template>
 
 <style>
@@ -7,7 +7,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
