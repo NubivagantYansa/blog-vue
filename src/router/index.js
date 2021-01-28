965860032f2ded0efbc776fa708fb475eade2dd0
“Debora <debora.crescenzo@gmail.com>
2021-01-28 17:11:48 +0000

    firebase done

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Details from "../views/Details.vue";
 import Create from "../views/Create.vue";
+import Tag from "../views/Tag.vue";
+import RealTime from "../views/RealTime.vue";
 
 const routes = [
   {
@@ -19,6 +21,16 @@ const routes = [
     path: "/create",
     name: "Create",
     component: Create,
+  },
+  {
+    path: "/tags/:tag",
+    name: "Tag",
+    component: Tag,
+  },
+  {
+    path: "/realtime",
+    name: "RealTime",
+    component: RealTime,
   },
 ];
 
